276dbc6b03b7f57ebc88cc24e91ff38ed21f2529
Wassim Benzarti <wass11121996@gmail.com>
2019-10-26 00:31:24 +0000

    Final

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ express()
     .get("/", (req,res)=>{
         res.json({
             ok:true,
-            version:1.5,
+            version:1.6,
             serverIsOnline:true
         })
     })
